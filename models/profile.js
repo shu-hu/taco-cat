c7bc54799423729104dc0edcfa467493cced5fab
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-07-15 16:11:21 -0400

    create cat schema

--- models/profile.js
+++ models/profile.js
@@ -4,9 +4,19 @@ export {
   Profile
 }
 
+const catSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  breed: String
+}, {
+  timestamps: true
+})
+
+
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
+  cats: [catSchema]
 }, {
   timestamps: true
 })
